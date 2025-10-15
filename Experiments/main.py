d2482ff75d194d011bf4da56020aaac9cbea27b4
ChongXern <chongxern@gmail.com>
2025-10-16 01:39:32 +0800

    Add option to show plot lines

--- Experiments/main.py
+++ Experiments/main.py
@@ -3,5 +3,5 @@ from apriltag_detection.apriltag_cv_detection import *
 if __name__ == "__main__":
     at_detector = initialise_detector()
     cap = cv2.VideoCapture(0)
-    detect_apriltag_from_cv(cap, at_detector)
+    detect_apriltag_from_cv(cap, at_detector, is_plot=False)
     
